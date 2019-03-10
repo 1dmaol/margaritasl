5d6d6ab15f4e6d176ada5cd0a01c94d439b126b9
marc oller <marc97oller@gmail.com>
2019-03-10 19:00:32 +0100

    Responsive por mejorar

--- login.php
+++ login.php
@@ -26,7 +26,8 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Login</title>
+  <title>Login</title> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:300,400,700"rel="stylesheet'>
     <link rel="stylesheet" href="css/login.css">
 </head>
