801e7a2151eb3ec81f000eae6d8dd8e7792a0247
marc oller <marc97oller@gmail.com>
2019-03-21 23:06:30 +0100

    Base de datos

--- database/margarita.sql
+++ database/margarita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 09-03-2019 a las 16:22:01
+-- Tiempo de generación: 21-03-2019 a las 23:04:38
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 5.6.40
 
@@ -25,30 +25,167 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `parcelas`
+--
+drop database if exists margarita;
+create database margarita;
+use margarita;
+
+CREATE TABLE `parcelas` (
+  `id` int(11) NOT NULL,
+  `nombre` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `parcelas`
+--
+
+INSERT INTO `parcelas` (`id`, `nombre`) VALUES
+(1, 'Parcela paco'),
+(2, 'Parcela pepe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `parcelas_de_usuarios`
+--
+
+CREATE TABLE `parcelas_de_usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `id_parcela` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `parcelas_de_usuarios`
+--
+
+INSERT INTO `parcelas_de_usuarios` (`id_usuario`, `id_parcela`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `posiciones`
+--
+
+CREATE TABLE `posiciones` (
+  `id` int(11) NOT NULL,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL,
+  `tipo` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `posiciones`
+--
+
+INSERT INTO `posiciones` (`id`, `lat`, `lng`, `tipo`) VALUES
+(1, 39.0041, -0.22179, 'Vertice'),
+(2, 39.0086, -0.204591, 'Vertice'),
+(3, 39.0038, -0.199312, 'Vertice'),
+(4, 38.9926, -0.214954, 'Vertice'),
+(5, 38.9967, -0.21446, 'Sonda'),
+(6, 39.0025, -0.21063, 'Sonda'),
+(7, 38.9973, -0.161576, 'Vertice'),
+(8, 38.9897, -0.163732, 'Vertice'),
+(9, 38.9946, -0.170296, 'Vertice');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sondas`
+--
+
+CREATE TABLE `sondas` (
+  `id` int(11) NOT NULL,
+  `id_parcela` int(11) NOT NULL,
+  `id_posicion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `sondas`
+--
+
+INSERT INTO `sondas` (`id`, `id_parcela`, `id_posicion`) VALUES
+(1, 1, 5),
+(2, 1, 6);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
-CREATE DATABASE margarita;
-
-USE margarita;
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombre` text COLLATE latin1_spanish_ci NOT NULL,
-  `clave` text COLLATE latin1_spanish_ci NOT NULL,
-  `rol` text COLLATE latin1_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  `nombre` text CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `clave` text CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `email` text CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL,
+  `rol` text CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `clave`, `rol`) VALUES
-(1, 'admin', 'admin', 'administrador'),
-(2, 'pepe', 'pepe1', 'trabajador');
+INSERT INTO `usuarios` (`id`, `nombre`, `clave`, `email`, `rol`) VALUES
+(1, 'admin', 'admin', 'admin@margarita.com', 'administrador'),
+(2, 'pepe', 'pepe1', 'pepe@margarita.com', 'trabajador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vertices`
+--
+
+CREATE TABLE `vertices` (
+  `id` int(11) NOT NULL,
+  `id_parcela` int(11) NOT NULL,
+  `id_posicion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `vertices`
+--
+
+INSERT INTO `vertices` (`id`, `id_parcela`, `id_posicion`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 2, 7),
+(6, 2, 8),
+(7, 2, 9);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `parcelas`
+--
+ALTER TABLE `parcelas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `parcelas_de_usuarios`
+--
+ALTER TABLE `parcelas_de_usuarios`
+  ADD PRIMARY KEY (`id_usuario`,`id_parcela`),
+  ADD KEY `id_parcela` (`id_parcela`);
+
+--
+-- Indices de la tabla `posiciones`
+--
+ALTER TABLE `posiciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sondas`
+--
+ALTER TABLE `sondas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -57,14 +194,55 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `vertices`
+--
+ALTER TABLE `vertices`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `parcelas`
+--
+ALTER TABLE `parcelas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `posiciones`
+--
+ALTER TABLE `posiciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `sondas`
+--
+ALTER TABLE `sondas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `vertices`
+--
+ALTER TABLE `vertices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `parcelas_de_usuarios`
+--
+ALTER TABLE `parcelas_de_usuarios`
+  ADD CONSTRAINT `parcelas_de_usuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `parcelas_de_usuarios_ibfk_2` FOREIGN KEY (`id_parcela`) REFERENCES `parcelas` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
